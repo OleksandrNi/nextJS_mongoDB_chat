3808539d2b89a92ac1c112f0ff97f7ff9d87f35f
Oleksandr Nikytenko <o.nikytenko@gmail.com>
2023-06-07 14:34:39 +0300

    look for bug2

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+// import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string;
+// type Data = {
+//   name: string;
+// };
+
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//   res.status(200).json({ name: "John Doe" });
+// }
+import { NextRequest, NextResponse } from 'next/server';
+ 
+export const config = {
+  runtime: 'edge',
 };
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: "John Doe" });
-}
+ 
+export default (request: NextRequest) => {
+  return NextResponse.json({
+    name: `Hello, from ${request.url} I'm now an Edge Function!`,
+  });
+};
