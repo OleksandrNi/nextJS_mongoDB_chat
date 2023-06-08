dd626981b02c82e509d5444d9f21a08d2d38ae7a
Oleksandr Nikytenko <o.nikytenko@gmail.com>
2023-06-08 15:39:12 +0300

    fix import3

--- pages/forgot-password.tsx
+++ pages/forgot-password.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ForgotPasswordForm from "../components/ForgotPasswordForm";
+import ForgotPasswordForm from "../components/ForgotPasswordForm/ForgotPasswordForm";
 
 const forgotPassword = () => {
   return <ForgotPasswordForm />;
